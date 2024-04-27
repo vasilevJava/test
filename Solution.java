bc9368eb87a3b74fdc040954d1fc61c3f12fb367
Maksim <167906677+vasilevJava@users.noreply.github.com>
2024-04-27 04:25:21 +0300

    fill an array

--- Solution.java
+++ Solution.java
@@ -1,4 +1,19 @@
+import java.util.Arrays;
+
 public class Solution {
+
+    public static int[] array = new int[9];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
     public static void main(String[] args) {
+        if (array.length % 2 == 0){
+            Arrays.fill(array, 0, (array.length / 2), valueStart);
+            Arrays.fill(array, (array.length / 2), array.length, valueEnd);
+        } else {
+            Arrays.fill(array, 0, (array.length / 2 + (array.length % 2)), valueStart);
+            Arrays.fill(array, (array.length / 2 + (array.length % 2)), array.length, valueEnd);
+        }
+            System.out.println(Arrays.toString(array));
     }
 }
