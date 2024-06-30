7e0e47f995f4c4c4374157d65e4cac32f82d38b2
Maksim <167906677+vasilevJava@users.noreply.github.com>
2024-06-30 07:13:38 +0300

    fix static mastake
    Static modifiers are placed incorrectly in the task

--- Solution.java
+++ Solution.java
@@ -1,19 +1,40 @@
 import java.util.Arrays;
 
 public class Solution {
-
-    public static int[] array = new int[9];
-    public static int valueStart = 10;
-    public static int valueEnd = 13;
-
     public static void main(String[] args) {
-        if (array.length % 2 == 0){
-            Arrays.fill(array, 0, (array.length / 2), valueStart);
-            Arrays.fill(array, (array.length / 2), array.length, valueEnd);
-        } else {
-            Arrays.fill(array, 0, (array.length / 2 + (array.length % 2)), valueStart);
-            Arrays.fill(array, (array.length / 2 + (array.length % 2)), array.length, valueEnd);
+        int[] array = {123, 7, -14, 67, 16, 98, 75, 576};
+        System.out.println("В массиве: ");
+        printArray(array);
+        System.out.println("Минимальное число: " + getMinValue(array));
+        System.out.println("Максимальное число: " + getMaxValue(array));
+
+    }
+
+    public static int getMaxValue(int[] array) {
+        int max = Integer.MIN_VALUE;
+        for (int value : array) {
+            if (value > max) {
+                max = value;
+            }
         }
-            System.out.println(Arrays.toString(array));
+        return max;
+    }
+
+    public static int getMinValue(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int value : array) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
+
