8fd68e515180d95b31869b7fa4875136166f4e73
Maksim <167906677+vasilevJava@users.noreply.github.com>
2024-07-21 01:18:47 +0300

    Update Solution.java
    correcting a mistake

--- Solution.java
+++ Solution.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 
 public class Solution {
-    byte b = 0;
-    short s = 1;
-    int i = 2;
-    long l = 3;
-    float f = 4;
-    double d = 5;
-    boolean b = fasle;
-    chat c = 7;//напишите тут ваш код
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    boolean g;
+    chat c;//напишите тут ваш код
 }
 
