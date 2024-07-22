fa7ed6bd3ee61bf8bbc845bf75b1feeb3082ddfd
Maksim <167906677+vasilevJava@users.noreply.github.com>
2024-07-23 00:16:43 +0300

    Solution.java
    Exponential number format

--- Solution.java
+++ Solution.java
@@ -1,28 +1,9 @@
 import java.util.Arrays;
 
 public class Solution {
-    public static char char1 = 101;
-    public static char char2 = 'H';
-    public static char char3 = 114;
-    public static char char4 = '\u006F';
-    public static char char5 = 108;
-    public static char char6 = 'h';
-    public static char char7 = 0x0063;
-    public static char char8 = ' ';
-    public static char char9 = 'l';
-    public static char char10 = 0x61;
-
-    public static void main(String[] args) {
-        System.out.print(char2);
-        System.out.print(char1);
-        System.out.print(char9);
-        System.out.print(char5);
-        System.out.print(char4);
-        System.out.print(char8);
-        System.out.print(char7);
-        System.out.print(char6);
-        System.out.print(char10);
-        System.out.print(char3);
-        System.out.println();
-    }
+    double earthDiameter = 1.2742E+4;
+    double lightSpeed = 2.99792458E+8;
+    double uraniumAtomicMass = 2.380289E+2;
+    double averageBeeWeight = 8.5E-2;
+    double javaDeveloperSalary = 1.0E+4;
 }
