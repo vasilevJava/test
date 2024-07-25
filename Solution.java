62023aab1568bf25c28bac72f3f3faefb4b3ed41
Maksim <167906677+vasilevJava@users.noreply.github.com>
2024-07-25 23:52:32 +0300

    Solution.java
    min value

--- Solution.java
+++ Solution.java
@@ -1,9 +1,8 @@
 import java.util.Arrays;
 
 public class Solution {
-    double earthDiameter = 1.2742E+4;
-    double lightSpeed = 2.99792458E+8;
-    double uraniumAtomicMass = 2.380289E+2;
-    double averageBeeWeight = 8.5E-2;
-    double javaDeveloperSalary = 1.0E+4;
+    public byte a = Byte.MIN_VALUE;
+    public short b = Short.MIN_VALUE;
+    public int c = Integer.MIN_VALUE;
+    public long d = Long.MIN_VALUE;
 }
