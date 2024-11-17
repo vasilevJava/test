ded9f44a8c747972e04876f952d86bf3aecf1eaa
Maksim <167906677+vasilevJava@users.noreply.github.com>
2024-11-17 07:58:25 +0300

    Update Solution.java
    return random number

--- Solution.java
+++ Solution.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
 
-public class Solution {
-    public byte a = Byte.MIN_VALUE;
-    public short b = Short.MIN_VALUE;
-    public int c = Integer.MIN_VALUE;
-    public long d = Long.MIN_VALUE;
+class Solution {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Math.random());
+        }
+    }
 }
